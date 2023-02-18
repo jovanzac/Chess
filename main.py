908c6884010ba6d0d36ea2fec9ccf3a0f6486ec7
jovanzac <jovanzac@gmail.com>
2023-02-18 21:42:38 +0530

    added rules to the chess pieces

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class Control :
         self.load_assets()
         self.set_board(0)
         self.selected = None, None
+        self.possible_moves = []
         self.turn = self.white_pieces_pos
 
 
@@ -83,18 +84,75 @@ class Control :
     def piece_move(self, piece, pos) :
         def limit(seq) :
             return [i for i in seq if i[0] in range(0, 8) and i[1] in range(0, 8) and i != pos]
+        def free_space(seq) :
+            return [i for i in seq if self.scan_board(i[0], i[1]) not in self.turn or self.scan_board(i[0], i[1]) == None]
+        def loop(condition, i, j, a, b) :
+            opp = self.black_pieces_pos if self.turn == self.white_pieces_pos else self.white_pieces_pos
+            while(free_space(limit([[i, j]]))) :
+                print(f"i, j is : {i, j}")
+                ret.append([i, j])
+                if condition(i, j) in opp :
+                    print("in condition")
+                    break
+                i += a
+                j += b
+
+        ret = []
+        # If piece is a king
         if piece in [self.b_king, self.w_king] :
-            return limit([[i, j]for i in range(pos[0]-1, pos[0]+2) for j in range(pos[1]-1, pos[1]+2)])
-        # elif piece == self.b_pawn :
-        #     return limit([[i, j] for ])
+            ret = free_space(limit([[i, j]for i in range(pos[0]-1, pos[0]+2) for j in range(pos[1]-1, pos[1]+2)]))
+        # If piece is a pawn
+        elif piece in [self.b_pawn, self.w_pawn] :
+            ret = free_space(limit([[pos[0]-1, pos[1]-1], [pos[0]-1, pos[1]], [pos[0]-1, pos[1]+1]]))
+            if not self.scan_board(pos[0]-1, pos[1]-1) and [pos[0]-1, pos[1]-1] in ret :
+                ret.remove([pos[0]-1, pos[1]-1])
+            if not self.scan_board(pos[0]-1, pos[1]+1) and [pos[0]-1, pos[1]+1] in ret :
+                ret.remove([pos[0]-1, pos[1]+1])
+        # If piece is a bishop
+        elif piece in [self.b_bishop, self.w_bishop] :
+            i, j = pos[0]-1, pos[1]-1
+            loop(lambda i, j:self.scan_board(i, j), i, j, -1, -1)
+            j = pos[1]+1
+            loop(lambda i, j:self.scan_board(i, j), i, j, -1, 1)
+            i, j = pos[0]+1, pos[1]-1
+            loop(lambda i, j:self.scan_board(i, j), i, j, 1, -1)
+            j = pos[1]+1
+            loop(lambda i, j:self.scan_board(i, j), i, j, 1, 1)
+        # If piece is a rook
+        elif piece in [self.b_rook, self.w_rook] :
+            i, j = pos[0]-1, pos[1]
+            loop(lambda i, j:self.scan_board(i, j), i, j, -1, 0)
+            i = pos[0]+1
+            loop(lambda i, j:self.scan_board(i, j), i, j, 1, 0)
+            i, j = pos[0], pos[1]-1
+            loop(lambda i, j:self.scan_board(i, j), i, j, 0, -1)
+            j = pos[1]+1
+            loop(lambda i, j:self.scan_board(i, j), i, j, 0, 1)
+        # If piece is a queen
+        elif piece in [self.b_queen, self.w_queen] :
+            ret += self.piece_move(self.b_bishop, pos)
+            ret += self.piece_move(self.b_rook, pos)
+        # If piece is a knight
+        elif piece in [self.b_knight, self.w_knight] :
+            ret = free_space(limit([
+                [pos[0]-2, pos[1]+1], [pos[0]-2, pos[1]-1], [pos[0]-1, pos[1]+2], [pos[0]-1, pos[1]-2],
+                [pos[0]+2, pos[1]+1], [pos[0]+2, pos[1]-1], [pos[0]+1, pos[1]+2], [pos[0]+1, pos[1]-2]
+            ]))
+
+        print(f"returned value is : {ret}")
+        return ret
 
 
     def click_handle(self, loc) :
-        ret = self.scan_board(loc[0], loc[1])
-        if ret in self.turn :
-            self.selected = (ret, loc)
-        elif self.selected[1] :
+        piece = self.scan_board(loc[0], loc[1])
+        if piece in self.turn :
+            self.selected = (piece, loc)
+            self.possible_moves = self.piece_move(piece, loc)
+        elif self.selected[1] and loc in self.possible_moves :
+            self.turn[self.selected[0]][1].remove(self.selected[1])
+            self.turn[self.selected[0]][1].append(loc)
             self.selected = None, None
+            self.possible_moves = []
             self.set_board(-1)
 
 
